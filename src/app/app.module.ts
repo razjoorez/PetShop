a3fcc2d016d33313bc99b2920553157a02532f24
razjoorez <46075716+razjoorez@users.noreply.github.com>
2021-04-03 15:17:23 +0100

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
 import { MaterialModule } from './shared/material.module';
 import { FormsModule } from '@angular/forms';
 import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavComponent,
-    MainComponent
+   // NavComponent,
+    MainComponent,
+   NavComponent,
+   MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
